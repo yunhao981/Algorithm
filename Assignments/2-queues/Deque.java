b5c762d275bb0045c9eeabc1b65957fbe04f4d27
Yunhao Liu <yunhao981@hotmail.com>
2019-07-01 01:28:18 +0800

    init ass 2

---
+++ Assignments/2-queues/Deque.java
@@ -0,0 +1,27 @@
+public class Deque<Item> implements Iterable<Item> {
+
+    public Deque() {
+
+    }
+    public boolean isEmpty() {
+
+    }
+    public int size() {
+
+    }
+    public void addFirst(Item item) {
+
+    }
+    public void addLast(Item item) {
+
+    }
+    public Item removeFirst() {
+
+    }
+    public Item removeLast() {
+
+    }
+    public Iterator<Item> iterator() {
+
+    }
+}
